66fcd51398b9cd37489d712a717ecb84efda98cd
unknown <ridoy.starlord@gmail.com>
2023-02-11 23:58:56 +0600

    Solved Above the Main Diagonal

---
+++ 1183.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+
+    double m[12][12];
+
+    int n;
+    char ch;
+    cin>>ch;
+
+    for (int i = 0; i < 12; i++)
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            double temp;
+            cin>>temp;
+            m[i][j]=temp;
+        }
+        
+    }
+    double ans=0,diagonal=1,count=0;;
+    if (ch=='S')
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            for (int i = diagonal; i < 12; i++)
+            {
+                ans+=m[j][i];
+            }
+            diagonal++;
+        }
+        cout<<fixed<<setprecision(1)<<ans<<endl;
+    }
+    if (ch=='M')
+    {
+        for (int j = 0; j < 12; j++)
+        {
+            for (int i = diagonal; i < 12; i++)
+            {
+                ans+=m[j][i];
+                count++;
+            }
+            diagonal++;
+        }
+        cout<<fixed<<setprecision(1)<<ans/count<<endl;
+    }
+   
+    // }
+
+    return 0;
+}
